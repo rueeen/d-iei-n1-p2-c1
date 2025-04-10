272f96b5b0475692135387120874ece78fb02bb0
rvalencia <rva.valencia@gmail.com>
2025-04-10 15:04:56 -0400

    feat: data types

---
+++ ejercicio03.py
@@ -0,0 +1,32 @@
+# Tipos de datos
+
+# Tipo cadena: string o str
+nombre = 'Una cadena de texto'
+variable = "Otra cadena de texto"
+# moe's
+variable_con_comillas = "moe's"
+sobrenombre = 'Alexis "el niño maravilla" Sanchez'
+# Este texto tiene "comillas dobles" y 'comillas simples'
+texto = "Este texto tiene \"comillas dobles\" y 'comillas simples'"
+#error = 'Esto da error"
+
+# Tipo entero: integer o int
+num = 10
+num = -5
+num = 30
+num = 1_0_0_0 # 1000 int
+noNumero = '10' # str
+
+# Tipo real: float VAN CON PUNTO
+num = 3.0 # float 
+num = 3.5
+num = 4.8
+num = .7 # 0.7
+num = 7.0 
+noNumero = 3,5 # No marca error, pero no es float es Tuple
+print(noNumero)
+
+# Tipo logico: boolean o bool
+valido = True
+#error = true # Esto da error
+valido = False
